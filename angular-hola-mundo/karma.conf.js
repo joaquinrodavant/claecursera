f8a444b1b4eaba025609748866cb7bf2ff79f43c
faroddav <facu.rod.dav@hotmail.com>
2021-09-21 11:40:12 -0300

    primera app

--- angular-hola-mundo/karma.conf.js
+++ angular-hola-mundo/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-nuevo-mundo'),
+      dir: require('path').join(__dirname, './coverage/angular-hola-mundo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
