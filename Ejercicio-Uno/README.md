8ce78e7a34a4bf38ab7fe22f939ef9d3ec33d61f
faroddav <facu.rod.dav@hotmail.com>
2021-09-22 10:42:35 -0300

    segundaapp

--- Ejercicio-Uno/README.md
+++ Ejercicio-Uno/README.md
@@ -1,4 +1,4 @@
-# AngularHolaMundo
+# EjercicioUno
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.6.
 
--- Ejercicio-Uno/src/app/app.component.spec.ts
+++ Ejercicio-Uno/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-hola-mundo'`, () => {
+  it(`should have as title 'Ejercicio-Uno'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-hola-mundo');
+    expect(app.title).toEqual('Ejercicio-Uno');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-hola-mundo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Ejercicio-Uno app is running!');
   });
 });
